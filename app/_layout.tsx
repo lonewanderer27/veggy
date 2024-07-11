4fbec70e515c25c17ca6c78ebc21a97ee816bdb5
lonewanderer27 <adrianejames27@gmail.com>
2024-07-11 09:35:55 +0800

    change to drawer layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,20 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
 import 'react-native-reanimated';
 
+import * as SplashScreen from 'expo-splash-screen';
+
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+
+import { Drawer } from 'expo-router/drawer';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Stack } from 'expo-router';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useEffect } from 'react';
+import { useFonts } from 'expo-font';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+export default function DrawerLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -28,10 +32,23 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Drawer>
+          <Drawer.Screen
+            name="index"
+            options={{
+              title: 'Home',
+            }}
+          />
+          <Drawer.Screen
+            name="explore"
+            options={{
+              title: 'Explore',
+            }}
+          />
+        </Drawer>
+      </GestureHandlerRootView>
     </ThemeProvider>
-  );
+
+  )
 }
